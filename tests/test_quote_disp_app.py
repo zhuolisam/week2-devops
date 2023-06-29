44fa1ffebd9f5b2a235322663893124289166978
zhuolisam <zhuolisam0627@gmail.com>
2023-06-29 15:26:27 +0800

    feat:extra features

---
+++ tests/test_quote_disp_app.py
@@ -0,0 +1,20 @@
+import pytest
+from quote_gen.app import app
+
+
+@pytest.fixture()
+def test_client():
+    with app.test_client() as testing_client:
+        with app.app_context():
+            yield testing_client 
+
+
+def test_home_page(test_client):
+    response = test_client.get('/')
+    assert response.status_code == 200
+
+
+def test_health(test_client):
+    response = test_client.get('/health')
+    assert response.status_code == 200
+    assert b"healthy" in response.data
